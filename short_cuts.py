Fixed average wavelength ID and completed the distortion function.

short_cuts.py
@@ -1,21 +1,21 @@
 import wsm
-from constants import *
-import numpy as np
-import xml_parser as xml
+
 import pylab as plt
-import optics as o
+import numpy as np
 
 
 def launch_task(task):  
+    global wsm
     
     if task==1: # Plot solar SED
         a=wsm.do_sed_map(SEDMode = SED_MODE_SOLAR)
         wsm.do_plot_sed_map(a)
         
     elif task==2: #Create and plot CCD map from continuous source 
-        a = wsm.do_sed_map(minLambda=0.3, maxLambda=0.9) 
-        b = wsm.do_ccd_map(a, 'hermes.xml')
-        wsm.do_plot_ccd_map(b, canvasSize=10)
+        a = wsm.do_sed_map(minLambda=0.4708, maxLambda=0.4893, deltaLambda=0.00001) 
+        b = wsm.do_ccd_map(a, 'hermes_b.xml')
+        wsm.do_load_spec('hermes_b.xml')
+        wsm.do_plot_ccd_map(b)
     
     elif task==3: #Read a calibration fits file
         '''Extract centroids.
@@ -25,7 +25,8 @@ def launch_task(task):
         calibrationImageFileName = 'hg_rhea_sample1.fits'
         specXMLFileName = 'rhea.xml'
         outputFileName = 'hg_rhea_sample1.txt'
-        wsm.do_read_calibration_file(calibrationImageFileName, specXMLFileName, outputFileName, booAvgAdjust = True, booPlotInitialPoints = False, booPlotFinalPoints = True)
+        wsm.do_load_spec(specXMLFileName)
+        wsm.do_read_calibration_file(calibrationImageFileName, specXMLFileName, outputFileName, booAvgAdjust = True, booPlotInitialPoints = True, booPlotFinalPoints = True)
        
     elif task==4: #Find fit
         calibrationDataFileName = 'c_hg_rhea_sample1.txt'
@@ -35,7 +36,7 @@ def launch_task(task):
         diagTry = [1.5, 0.5, 0.9 , 0.2, 1.2, 1.2, 1.6, 1, 1.2, 6.7, 50, 50] 
         showStats = True
 
-        SEDMap = wsm.do_sed_map(SEDMode=SED_MODE_FILE, spectrumFileName='hg_spectrum.txt') #create SEDMap from flat Hg emission file
+        SEDMap = wsm.do_sed_map(SEDMode=wsm.SED_MODE_FILE, spectrumFileName='hg_spectrum.txt') #create SEDMap from flat Hg emission file
         p_out = wsm.do_find_fit(SEDMap, specXMLFileName, calibrationDataFileName, activeCamera, diagTry = diagTry, showStats = showStats)      
         
         #plot fit to see new model
@@ -46,9 +47,9 @@ def launch_task(task):
         #wsm.do_plot_sed_map(SEDMap, title='Hg SED raw input')
         
         #step 1 - Read fits, extract calibration points to c_+outputFileName (n x 5 np.array) 
-        calibrationImageFileName = 'hg_rhea_sample2.fits'
-        outputFileName = 'hg_rhea_sample2.txt'
-        specXMLFileName =  'rhea.xml'
+        calibrationImageFileName = 'hg_rhea_sample1.fits'
+        specXMLFileName = 'rhea.xml'
+        outputFileName = 'hg_rhea_sample1.txt'
         wsm.do_read_calibration_file(calibrationImageFileName, specXMLFileName, outputFileName)
         
         #step 2 - find best fit to results extracted
@@ -109,6 +110,12 @@ def launch_task(task):
         plt.ioff()
         plt.show()
         
+    elif task==9: #Create and plot CCD map from continuous source 
+        a = wsm.do_sed_map(minLambda=0.3, maxLambda=0.9, deltaLambda=0.0001) 
+        b = wsm.do_ccd_map(a, 'rhea.xml')
+        wsm.do_load_spec('rhea.xml')
+        wsm.do_plot_ccd_map(b, backImage='sky_rhea_sample.fits')
+        
     elif task==100: #random stuff
         a=wsm.do_sed_map(SEDMode=SED_MODE_FILE, specFile = 'hg_spectrum.txt')
     #    diag_try = [10,1,1,1,1,1,1,1,1,1,1]
@@ -118,12 +125,12 @@ def launch_task(task):
         print 'Finished'
 
     elif task==101: #Distortion tests
-        inX = np.arange(-50,51)
+        inX = np.arange(-20,21)
         inX = np.tile(inX,len(inX))
         inY = np.sort(inX)
-        K=-0.3
+        K=[-0.8,0,0.5]
         
-        outX, outY = o.distort(inX, inY, K, 20, 20)
+        outX, outY = wsm.wt.distort(inX, inY, K, 4, -7)
         
         plt.scatter(outX, outY)
         plt.show()
@@ -132,7 +139,11 @@ def launch_task(task):
         import wsmtools as wt
         a = wt.xml.find_specXMLFileName('rhea.xml')
         
-launch_task(7)
+    elif task==103: #variable scope
+        import wsm
+        wsm.do_load_spec('rhea.xml')
+        
+launch_task(5)
 
 
 #a=wsm.do_sed_map(minLambda=0.3, maxLambda=0.9)

wsm.py
@@ -1,25 +1,20 @@
-#General Imports
-import random       #random generator package
-import pyfits       #fits package
-import os           #os access
-
-#Aliases
+#external Packages
+import random        
+import pyfits       
+import os           
 import numpy as np
-import matplotlib.cm as cm
 import bisect as bis
+import matplotlib.cm as cm
 import matplotlib.image as mpimg
-import matplotlib.pyplot as plt     #matlab
+import matplotlib.pyplot as plt    
 from scipy.optimize.minpack import leastsq
-from  scipy.optimize import * #least square package
+from scipy.optimize import * #least square package
 from scipy import interpolate #interpolate function
 from astLib import astSED #Astro Libraries           
-import xml_parser as xml
 
-#Custom Packages
-from constants import *
-import xml_parser
+#internal modules
+from wsmtools.constants import *
 import wsmtools as wt
-from optics import distort
 
 def do_sed_map(SEDMode=SED_MODE_FLAT, minLambda=0.4, maxLambda=0.78, deltaLambda=0.0001, intNormalize=0, spectrumFileName=''): 
     '''
@@ -125,32 +120,23 @@ def do_ccd_map(SEDMap ,specXMLFileName, activeCamera=0, p_try = []):
     '''   
             
     #Reads xml file
-    Beams, Optics, Cameras, p, stheta = xml_parser.read_all(specXMLFileName, p_try)
-#    print p_try
-#    if p_try != []: p = p_try  #when p comes from a fitting result overwrites the p read from the xml file
+    Beams, Optics, Cameras, p, stheta = wt.xml.read_all(specXMLFileName, p_try)   
     
-    
-    #hack for RHEA. Needs manual reverse prism on beam return. todo
+    #hack for RHEA. Needs manual reverse of prism on beam return. todo
     if specXMLFileName=='rhea.xml':
         Optics[4][0]=-Optics[0][0]
         Optics[3][0]=-Optics[1][0]  
     
     #Launch grid loop. Creates an array of (x,y,lambda, Intensity, Order)
-    CCDX, CCDY, CCDLambda, CCDIntensity, CCDOrder = wt.optics.ccd_loop(SEDMap, Beams , Optics, Cameras[activeCamera], stheta) #minLambda ,maxLambda ,deltaLambda ,minOrder ,maxOrder ,deltaOrder ,fLength ,stheta) 
+    CCDX, CCDY, CCDLambda, CCDIntensity, CCDOrder, CCDBeamID = wt.optics.ccd_loop(SEDMap, Beams , Optics, Cameras[activeCamera], stheta)
      
     #Distort if any distortion data present
-    K = p[11]
-    Xc = p[12]
-    Yc = p[13]
-#    import pylab as kkk
-#    import sys
-#    kkk.scatter(CCDX, CCDY)
-#    CCDX, CCDY = distort(CCDX, CCDY, K)
-#    kkk.scatter(CCDX, CCDY , color= 'red')
-#    kkk.ioff()
-#    kkk.show()
-
-    return CCDX, CCDY, CCDLambda, CCDIntensity, CCDOrder
+    K = [p[11], p[12], p[13]]
+    Xc = p[14]
+    Yc = p[15]
+    CCDX, CCDY = wt.distort(CCDX, CCDY, K, Xc, Yc)
+    
+    return CCDX, CCDY, CCDLambda, CCDIntensity, CCDOrder, CCDBeamID
 
 def do_find_fit(SEDMap, specXMLFileName, calibrationDataFileName, activeCameraIndex, p_try = [], factorTry=1 ,diagTry = [], showStats = False, maxfev = 1000, booWriteP = True):
     '''
@@ -174,7 +160,7 @@ def do_find_fit(SEDMap, specXMLFileName, calibrationDataFileName, activeCameraIn
     #x,y,waveList,xsig,ysig = readCalibrationData(calibrationFile)
     #fit is the output, which is the ideal p vector.
     
-    if p_try==[]: p_try = xml.read_p(specXMLFileName)
+    if p_try==[]: p_try = wt.xml.read_p(specXMLFileName)
     if diagTry==[]: diagTry = np.ones(len(p_try))
     
     while True:
@@ -217,10 +203,12 @@ def do_read_calibration_file(calibrationImageFileName, specXMLFileName, outputFi
     Notes
     -----
     '''      
+    global Beams, Optics, Cameras, a, b 
+    
     if booAnalyse: wt.ia.analyse_image_sex(calibrationImageFileName, outputFileName)
     
     #Loads coordinate points from calibration output file
-    imageMapX, imageMapY, image_map_sigx, image_map_sigy = wt.load_image_map_sex(outputFileName)
+    imageMapX, imageMapY, image_map_sigx, image_map_sigy = wt.ia.load_image_map_sex(outputFileName)
     if imageMapX == []: return
     imageMapX -= int(Cameras[0][CamerasWidth])/2
     imageMapY -= int(Cameras[0][CamerasHeight])/2
@@ -255,7 +243,7 @@ def do_read_calibration_file(calibrationImageFileName, specXMLFileName, outputFi
     f.close()
 
     #Correct/confirm found wavelengths
-    imageMapLambda = wt.ia.identify_imageMapLambda_manual(SEDMap, CCDX, CCDY, CCDLambda, imageMapX, imageMapY, imageMapLambda, calibrationImageFileName)
+#    imageMapLambda = wt.ia.identify_imageMapLambda_manual(SEDMap, CCDX, CCDY, CCDLambda, imageMapX, imageMapY, imageMapLambda, calibrationImageFileName, Cameras)
 
     #Create final output file with all calibration data
     f = open(TEMP_PATH + 'c_' + outputFileName,'w')
@@ -291,19 +279,23 @@ def do_extract_order(CCDMap, nOrder, image):
     
     return flux, newLambdas
 
-def do_plot_ccd_map(CCDMap, canvasSize=2, backImage=''):
+def do_plot_ccd_map(CCDMap, canvasSize=1, backImage=''):
         
         CCDX = CCDMap[CCD_MAP_X] 
         CCDY = CCDMap[CCD_MAP_Y] 
         CCDLambda = CCDMap[CCD_MAP_LAMBDA] 
         CCDIntensity = CCDMap[CCD_MAP_INTENSITY] 
         CCDOrder = CCDMap[CCD_MAP_ORDER] 
+        CCDBeamID = CCDMap[CCD_MAP_BEAMID] 
 
         colorTable = np.array((wt.wav2RGB(CCDLambda, CCDIntensity))) 
         
-        imWidth = int(Cameras[0][CamerasWidth])/2
-        imHeight = int(Cameras[0][CamerasHeight])/2
+        imWidth = int(Cameras[0][CamerasWidth])
+        imHeight = int(Cameras[0][CamerasHeight])
         
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111, axisbg = 'black')
+
         if backImage!='':
             im = pyfits.getdata(FITS_PATH + backImage)
             im[im<0] = 0
@@ -326,8 +318,7 @@ def do_plot_ccd_map(CCDMap, canvasSize=2, backImage=''):
             plt.imshow(im,extent=[-imWidth/2 , imWidth/2 , -imHeight/2 , imHeight/2])
             plt.set_cmap(cm.Greys_r)
         
-        fig = plt.figure()
-        ax1 = fig.add_subplot(111, axisbg = 'black')
+
 
         ax1.scatter(CCDX, -CCDY ,s=8, color=colorTable , marker='o', alpha =.5)
         plt.axis([-imWidth/2 * canvasSize , imWidth/2 * canvasSize , -imHeight/2 * canvasSize , imHeight/2 * canvasSize])
@@ -380,7 +371,7 @@ def do_plot_calibration_points(backImageFileName, calibrationDataFileName, CCDMa
         imWidth = hdulist[0].header['NAXIS1']
         imHeight = hdulist[0].header['NAXIS2']
         im = pyfits.getdata(FITS_PATH + backImageFileName) 
-        imNorm = ic.normalise_image(im) 
+        imNorm = wt.ic.normalise_image(im) 
 #        plt.ion()
         fig = plt.figure()
         ax1 = fig.add_subplot(111)
@@ -430,6 +421,9 @@ def do_plot_calibration_points(backImageFileName, calibrationDataFileName, CCDMa
 #        plt.draw()
         plt.show()
         
-        
-Beams, Optics, Cameras, a, b = xml.read_all('rhea.xml')
-  
+def do_load_spec(specXMLFileName):        
+    global Beams, Optics, Cameras, a, b 
+    
+    Beams, Optics, Cameras, a, b = wt.xml.read_all(specXMLFileName)
+
+#do_load_spec('hermes.xml')

wsmtools/image_analysis.py
@@ -1,3 +1,12 @@
+import numpy as np
+import subprocess
+import pyfits
+import pylab as plt
+import matplotlib.cm as cm
+
+from constants import *
+import image_calibration as ic
+
 def analyse_image_sex(image_filename='test.fits', image_map_filename='image_map.txt'):
       
     os_command = SEXTRACTOR_PATH +'sex ' + FITS_PATH + image_filename + ' -c ' + SPEC_PATH + 'rhea.sex'
@@ -19,8 +28,18 @@ def identify_imageMapLambda_auto(SEDMap, CCDX, CCDY, CCDLambda, imageMapX, image
         avgImageY = np.average(imageMapY)
         avgCCDX = np.average(CCDX)
         avgCCDY = np.average(CCDY)
-        CCDXShifted =  CCDX + (avgCCDX - avgImageX)
-        CCDYShifted =  CCDY + (avgCCDY - avgImageY)
+        CCDXShifted = CCDX + (avgImageX- avgCCDX)
+        CCDYShifted = CCDY + (avgImageY - avgCCDY)
+
+        import pylab as ppp
+        ppp.scatter(CCDX, CCDY, s=50, color="blue", label='Model Data')
+        ppp.scatter(imageMapX, imageMapY, s=50, color="red", label='Calibration Data')
+        ppp.scatter(CCDXShifted, CCDYShifted, s=50, color="green", label='Average Corrected Model Data')
+        ppp.show()
+        
+        CCDX = CCDXShifted 
+        CCDY = CCDYShifted
+        
         
     for i in range(len(imageMapX)):
         
@@ -36,12 +55,11 @@ def identify_imageMapLambda_auto(SEDMap, CCDX, CCDY, CCDLambda, imageMapX, image
     
     return imageMapLambda
 
-def identify_imageMapLambda_manual(SEDMap, CCDX, CCDY, CCDLambda, imageMapX, imageMapY, imageMapLambda, backImageFileName, canvasSize = 1):    
-    global Beams, Optics, Cameras
-        
+def identify_imageMapLambda_manual(SEDMap, CCDX, CCDY, CCDLambda, imageMapX, imageMapY, imageMapLambda, backImageFileName, Cameras, canvasSize = 1):    
+    
     #Plot
-    imWidth = int(wsm.Cameras[0][CamerasWidth])
-    imHeight = int(wsm.Cameras[0][CamerasHeight])
+    imWidth = int(Cameras[0][CamerasWidth])
+    imHeight = int(Cameras[0][CamerasHeight])
     im = pyfits.getdata(FITS_PATH + backImageFileName) 
     imNorm = ic.normalise_image(im) 
     

wsmtools/optics.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from constants import *
+
 def ccd_loop(SEDMap, Beams, Optics, Camera, stheta): #, intNormalize,Interpolate=False,BackImage='',GaussFit=False):
     '''
     Computes the projection of n beams of monochromatic light passing through an optical system. 
@@ -30,11 +32,11 @@ def ccd_loop(SEDMap, Beams, Optics, Camera, stheta): #, intNormalize,Interpolate
     '''   
        
     dataOut=np.zeros(5)
-    CCDX = CCDY = CCDLambda = CCDIntensity = CCDOrder = np.array([])
+    CCDX = CCDY = CCDLambda = CCDIntensity = CCDOrder = CCDBeamID = np.array([])
     
     pixelSize = float(Camera[CamerasPSize])
     fLength = float(Camera[CamerasFLength])
-    blaze_angle = stheta #Approximately np.arctan(2)
+    blaze_angle = stheta #Approximately np.arctan(2) #todo check how to make this general
 
     #Retrieves max and min lambdas for intensity calculation
     minLambda=min(SEDMap[SEDMapLambda])
@@ -42,6 +44,10 @@ def ccd_loop(SEDMap, Beams, Optics, Camera, stheta): #, intNormalize,Interpolate
 #    allFlux=np.array([0])
 #    allLambdas=np.array([0])
     
+    for i in range(len(Optics)):
+        if (Optics[i][OpticsType]==OpticsRGrating or Optics[i][OpticsType]==OpticsVPHGrating):
+            GPeriod=Optics[i][OpticsGPeriod]
+        
     #One per beam
     for Beam in Beams:
         
@@ -51,21 +57,20 @@ def ccd_loop(SEDMap, Beams, Optics, Camera, stheta): #, intNormalize,Interpolate
             #loop lambda for current order
             for i in np.arange(len(SEDMap[SEDMapLambda])): 
                 
-                Lambda=SEDMap[SEDMapLambda][i]
-                inI=SEDMap[SEDMapIntensity][i]
+                Lambda = SEDMap[SEDMapLambda][i]
+                inI = SEDMap[SEDMapIntensity][i]
                             
-                #rhea hack GPeriod from optics, todo
-                GPeriod=31.50321471
+                #rhea hack GPeriod from optics, fixed, remove after a while
+#                GPeriod=31.50321471
                 
                 #the wavelength range is from the blaze wavelength of the next order and the blaze wavelength of the previous order
-                if (Lambda >= abs(2*GPeriod*np.sin(blaze_angle)/(nOrder+1)) and Lambda <= abs(2*GPeriod*np.sin(blaze_angle)/(nOrder-1))):
-    
+#                if (Lambda >= abs(2*GPeriod*np.sin(blaze_angle)/(nOrder+1)) and Lambda <= abs(2*GPeriod*np.sin(blaze_angle)/(nOrder-1))):
+                if (abs(Lambda*(nOrder+1)) >= abs(2*GPeriod*np.sin(blaze_angle)) and abs(Lambda*(nOrder-1)) <= abs(2*GPeriod*np.sin(blaze_angle))):
+#                if 1==1:  
                     #Computes the unit vector that results from the optical system for a given wavelength and order
                     #This is the actual tracing of the ray for each wavelength             
-                    start_time = time.time()
-                    v, isValid = ray_trace_flex(Beam, Lambda, nOrder, Optics, blaze_angle)
-                    elapsed_time = time.time() - start_time
-                    #print 'Elepased time: ' + str(elapsed_time)
+                    v, isValid, beamID = ray_trace_flex(Beam, Lambda, nOrder, Optics, blaze_angle)
+
                     
                     if isValid: #no errors in calculation, within 1 order of blaze wavelength and beam makes it back through the prism
                         x=v[0]*fLength*1000/pixelSize # x-coord in focal plane in pixels
@@ -80,14 +85,16 @@ def ccd_loop(SEDMap, Beams, Optics, Camera, stheta): #, intNormalize,Interpolate
                             CCDLambda = np.array([Lambda])
                             CCDIntensity = np.array([inI*outI])
                             CCDOrder = np.array([nOrder])
+                            CCDBeamID = np.array([beamID])
                         else:
                             CCDX = np.append(CCDX,[x],0)
                             CCDY = np.append(CCDY,[z],0)
                             CCDLambda = np.append(CCDLambda,[Lambda],0)
                             CCDIntensity = np.append(CCDIntensity,[inI*outI],0)
                             CCDOrder = np.append(CCDOrder,[nOrder],0)
-        
-    return CCDX, CCDY, CCDLambda, CCDIntensity, CCDOrder
+                            CCDBeamID = np.append(CCDBeamID,[beamID],0)
+       
+    return CCDX, CCDY, CCDLambda, CCDIntensity, CCDOrder, CCDBeamID
 
 def ray_trace_flex(Beam, Lambda, nOrder, Optics, blaze_angle):
     ''' Traces a beam through the spectrograph. 
@@ -98,7 +105,8 @@ def ray_trace_flex(Beam, Lambda, nOrder, Optics, blaze_angle):
     l=grating, parallel to the grooves.
     d=blaze period'''
     
-    v = Beam
+    v = Beam[0]
+    beamID = Beam[1]
     
     #loops through optics array
     for i in range(len(Optics)):
@@ -122,7 +130,7 @@ def ray_trace_flex(Beam, Lambda, nOrder, Optics, blaze_angle):
             s=Optics[i][OpticsCoords1]
             l=Optics[i][OpticsCoords2]
             v_out, isValid = VPHGrating(v, s, l, nOrder, Lambda, GPeriod)
-            isValid = True
+#            isValid = True
 #            else:
 #                return v, isValid
             
@@ -146,7 +154,7 @@ def ray_trace_flex(Beam, Lambda, nOrder, Optics, blaze_angle):
 #            """Vector transform due to fourth surface"""
 #            u = Snell3D(nPrism, nAir, u, n5)
             
-    return v, isValid
+    return v, isValid, beamID
 
 def RGrating(u, s, l, nOrder, Lambda, d):
     """Computes the new direction of a vector when hitting a grating."""
@@ -234,18 +242,21 @@ def distort( inX, inY, K=0, Xc=0 , Yc=0):
     -----
     
     '''
-    
-    Delta=1
-    r = np.sqrt((inX-np.ones(len(inX))*Xc)**2+(inY-np.ones(len(inY))*Yc)**2)
-    r /= max(r)
-       
-#    for i in np.arange(len(K)):
-#        Delta += K[i] * r**((i+1)*2)
-    Delta += K * r**2
+    if len(inX)>0:
         
-    outX = (inX-np.ones(len(inX))*Xc)*Delta
-    outY = (inY-np.ones(len(inY))*Yc)*Delta
-
+        Delta=1
+        r = np.sqrt((inX-np.ones(len(inX))*Xc)**2+(inY-np.ones(len(inY))*Yc)**2)
+        r /= max(r)
+           
+        for i in np.arange(len(K)):
+            Delta += K[i] * r**((i+1)*2)
+        
+        outX = (inX-np.ones(len(inX))*Xc)*Delta
+        outY = (inY-np.ones(len(inY))*Yc)*Delta
+    
+    else:
+        outX, outY = inX, inY
+        
     return outX, outY
 
 def wav2RGB(Lambda, Intensity):

wsmtools/xml_parser.py
@@ -6,7 +6,7 @@ import wsmtools as wt
 
 def read_p(specXMLFileName):
     
-    p=np.zeros(14)
+    p=np.zeros(16)
     
     
     xmldoc = minidom.parse(SPEC_PATH + specXMLFileName)
@@ -60,7 +60,7 @@ def write_p(p, specXMLFileName):
     
 def read_all(specXMLFileName, p_in = []):
 
-    p=np.zeros(14)
+    p=np.zeros(16)
     Optics=np.array([])
     Beams=np.array([])
     Cameras=np.array([])
@@ -110,8 +110,12 @@ def read_all(specXMLFileName, p_in = []):
                                    minLambda=float(child.firstChild.data)
                                 elif child.nodeName=='maxLambda':
                                    maxLambda=float(child.firstChild.data)
-                                elif child.nodeName=='distortion':
-                                   distortion=float(child.firstChild.data)
+                                elif child.nodeName=='distortion1':
+                                   distortion1=float(child.firstChild.data)
+                                elif child.nodeName=='distortion2':
+                                   distortion2=float(child.firstChild.data)
+                                elif child.nodeName=='distortion3':
+                                   distortion3=float(child.firstChild.data)
                                 elif child.nodeName=='distortionCenterX':
                                    distortionCenterX=float(child.firstChild.data)
                                 elif child.nodeName=='distortionCenterY':
@@ -134,14 +138,18 @@ def read_all(specXMLFileName, p_in = []):
                                             minLambda = p_in[int(child.attributes.getNamedItem('param').value)]   
                                         elif child.nodeName=='maxLambda':
                                             maxLambda = p_in[int(child.attributes.getNamedItem('param').value)]   
-                                        elif child.nodeName=='distortion':
-                                            distortion = p_in[int(child.attributes.getNamedItem('param').value)]   
+                                        elif child.nodeName=='distortion1':
+                                            distortion1 = p_in[int(child.attributes.getNamedItem('param').value)]   
+                                        elif child.nodeName=='distortion2':
+                                            distortion2 = p_in[int(child.attributes.getNamedItem('param').value)]   
+                                        elif child.nodeName=='distortion3':
+                                            distortion3 = p_in[int(child.attributes.getNamedItem('param').value)]   
                                         elif child.nodeName=='distortionCenterX':
                                             distortionCenterX = p_in[int(child.attributes.getNamedItem('param').value)]   
                                         elif child.nodeName=='distortionCenterY':
                                             distortionCenterY = p_in[int(child.attributes.getNamedItem('param').value)]   
                                 
-                        newCamera=[[name, fLength, width, height, pSize, minLambda, maxLambda, distortion, distortionCenterX, distortionCenterY]]
+                        newCamera=[[name, fLength, width, height, pSize, minLambda, maxLambda, distortion1, distortion2, distortion3, distortionCenterX, distortionCenterY]]
                         
                         if len(Cameras)==0:
                             Cameras = np.array(newCamera)
@@ -152,11 +160,17 @@ def read_all(specXMLFileName, p_in = []):
             elif specElement.nodeName=='beams':
                 for beam in specElement.childNodes:   
                     if beam.nodeType==1:
+                        
                         if beam.hasAttribute('param'):
                             if p_in==[]:
                                 p[int(beam.attributes.getNamedItem('param').value)] = float(beam.firstChild.data)
                             else:
                                 p[int(beam.attributes.getNamedItem('param').value)] = p_in[int(beam.attributes.getNamedItem('param').value)]                    
+                        
+                        if beam.hasAttribute('beamID'):
+                            beamID = int(beam.attributes.getNamedItem('beamID').value)
+                   
+                        
                         for child in beam.childNodes:
                             if child.nodeType==1: 
                                 if child.nodeName=='phi':
@@ -174,12 +188,12 @@ def read_all(specXMLFileName, p_in = []):
                                         elif child.nodeName=='theta':
                                             theta = p_in[int(child.attributes.getNamedItem('param').value)]   
                                 
-                        newBeam=[[np.cos(np.radians(float(phi)))*np.sin(np.radians(float(theta))),np.sin(np.radians(float(phi)))*np.sin(np.radians(float(theta))),np.cos(np.radians(float(theta)))]]
+                        newBeam=[np.cos(np.radians(float(phi)))*np.sin(np.radians(float(theta))),np.sin(np.radians(float(phi)))*np.sin(np.radians(float(theta))),np.cos(np.radians(float(theta)))]
                         
                         if len(Beams)==0:
-                            Beams = np.array(newBeam)
+                            Beams = np.array([[newBeam, beamID]])
                         else:
-                            Beams = np.append(Beams, newBeam, 0)
+                            Beams = np.append(Beams, [[newBeam, beamID]], 0)
                             
             elif specElement.nodeName=='optical_elements':  
                 for optElement in specElement.childNodes:   